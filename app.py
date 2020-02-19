1f2de5ecd5704705f950c9759303a3cc3332a0a2
Luming Chen <lumingc@berkeley.edu>
2020-02-19 15:58:46 -0800

    add database for user inputs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import pymongo 
-from flask import Flask, render_template, jsonify
+import json
+from bson.objectid import ObjectId
+from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,6 +11,7 @@ CORS(app)
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["train_sketches"]
 col = db["big_stroke_sketches_and_z"]
+col2 = db["sketch_pairs_test"]
 
 @app.route("/api/load_sketches", methods=['GET'])
 def load_sketches():
@@ -19,13 +22,21 @@ def load_sketches():
     sketch_1["_id"] = str(sketch_1["_id"])
     sketch_2["_id"] = str(sketch_2["_id"])
     # make sure sketch 1 and 2 are not the same
-    while str(sketch_2["_id"]) == str(sketch_1["_id"]):
+    while sketch_2["_id"] == sketch_1["_id"]:
         sketch_2 = list(col.aggregate([{"$sample": {"size": 1}}]))[0]
     return jsonify(strokes1 = sketch_1["strokes"], z1 = sketch_1["z"], strokes2 = sketch_2["strokes"], z2 = sketch_2["z"])
 
 @app.route("/")
 def compare():
     return render_template("index.html")
+
+@app.route("/api/inputs", methods=['POST'])
+def submit_post():
+    if request.method == 'POST':
+        doc = json.loads(request.data)
+        col2.insert_one(doc)
+        return "post succeeded"
+    return "not a post"
 
 if __name__ == "__main__":
     app.config.from_object('configurations.DevelopmentConfig')
--- js/App.tsx
+++ js/App.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import {SketchCanvas} from "./sketch_canvas";
-import {Box, Grid} from '@material-ui/core';
+import {Box, Grid, TextField, Button, Typography, withStyles, StyledComponentProps} from '@material-ui/core';
 
-export class App extends React.Component<{},{strokes1: number[][], strokes2: number[][], user_input: ""}> {
+const styles = theme => ({
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  paper: {
+    marginTop: theme.spacing(1),
+    alignItems: 'center'
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    alignItems: 'right'
+  }
+});
 
-constructor(props) {
+class App extends React.Component<{} & StyledComponentProps,{strokes1: number[][], strokes2: number[][], user_input: ""}> {
+  strokes1_z: any;
+  strokes2_z: any;
+  constructor(props) {
   super(props);
   this.state = {
     strokes1:[],
@@ -24,8 +40,8 @@ componentDidMount() {
         strokes1: r['strokes1'],
         strokes2: r['strokes2']
       })
-      console.log(r['z1']);
-      console.log(r['z2']);
+      this.strokes1_z = r['z1']
+      this.strokes2_z = r['z2']
     })
     .catch(err => console.log(err))
 }
@@ -35,23 +51,40 @@ handleChange(event) {
 }
 
 handleSubmit(event) {
+  event.preventDefault();
+  var data = {};
+  data["sketch1_strokes"] = this.state.strokes1;
+  data["sketch1_z"] = this.strokes1_z;
+  data["sketch2_strokes"] = this.state.strokes2;
+  data["sketch2_z"] =  this.strokes2_z;
+  data["user_input"] = this.state.user_input;
+  fetch( "http://localhost:5000/api/inputs", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
   window.location.reload(false);
 }
 
 render() {
+    const { classes } = this.props;
     return this.state.strokes1.length? (
-    <div className="sketch">
-    <h1>Hello React!</h1>
+    <div className={classes!.paper}>
+    <Box paddingTop={2} paddingLeft={5} paddingRight={5}>
+      <Typography variant="h4" display="block" color='initial'>
+        How do the two sketches below differ?
+      </Typography>
+    </Box>
       <Box paddingTop={2} paddingLeft={2} paddingRight={2}>
         <Grid container justify="center" spacing={10}>
-          
           <Grid item xs={5}>
             <Box border={1} margin={1}>
             <SketchCanvas strokes={this.state.strokes1}
             sketchingComplete={() => {}}
             ></SketchCanvas> </Box>
           </Grid>      
-        
           <Grid item xs={5}>
             <Box border={1} margin={1}>
             <SketchCanvas strokes={this.state.strokes2}
@@ -60,13 +93,30 @@ render() {
           </Grid>
         </Grid>
       </Box>
-      <form onSubmit={this.handleSubmit}>
-          <label>
-            Please describe the transition from sktech on the left to sketch on the right:
-            <input type="text" value={this.state.user_input} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-      </form> 
+      <Box paddingTop={2} paddingLeft={5} paddingRight={5}>
+        <Typography variant="body1" display="block" color='initial'>
+        Please describe the transition from the sketch on the left to the sketch on the right (eg. "Make the leaves of a tree taller")
+        </Typography>
+        <form className={classes!.form}>
+          <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="description"
+                label="description"
+                name="description"
+                onChange={this.handleChange}
+              />
+          <Button className={classes!.submit}
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{justifyContent: 'right'}}
+            onClick={(event) => {this.handleSubmit(event);}}
+          >Submit</Button>
+        </form> 
+      </Box>
     </div>
     ):(
       <span>Loading sketches...</span>
@@ -74,4 +124,4 @@ render() {
   }
 }
 
-export default App;
+export default withStyles(styles, {withTheme: true})(App);
